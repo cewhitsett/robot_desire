554bedb475bdb9ef47983fb93d47dcb590687906
hmc-cs-cwhitsett <ewhitsett@hmc.edu>
2017-11-30 02:26:49 +0000

    Finished doc

--- PiMotor.py
+++ PiMotor.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python
+
+# Library for PiMotor Shield V2
+# Developed by: SB Components
+# Author: Ankur
+# Project: RPi Motor Shield
+
 import RPi.GPIO as GPIO                        #Import GPIO library
 import time
 from time import sleep
@@ -9,7 +16,7 @@ class Motor:
     ''' Class to handle interaction with the motor pins
     Supports redefinition of "forward" and "backward" depending on how motors are connected
     Use the supplied Motorshieldtest module to test the correct configuration for your project.
-
+    
     Arguments:
     motor = string motor pin label (i.e. "MOTOR1","MOTOR2","MOTOR3","MOTOR4") identifying the pins to which
             the motor is connected.
@@ -19,7 +26,7 @@ class Motor:
                  "MOTOR3":{"config":{1:{"e":19,"f":21,"r":23},2:{"e":19,"f":23,"r":21}}, "arrow":2},
                  "MOTOR2":{"config":{1:{"e":22,"f":16,"r":18},2:{"e":22,"f":18,"r":16}}, "arrow":3},
                  "MOTOR1":{"config":{1:{"e":11,"f":15,"r":13},2:{"e":11,"f":13,"r":15}},"arrow":4}}
-
+    
     def __init__(self, motor, config):
         self.testMode = False
         self.arrow = Arrow(self.motorpins[motor]["arrow"])
@@ -36,8 +43,8 @@ class Motor:
     def test(self, state):
         ''' Puts the motor into test mode
         When in test mode the Arrow associated with the motor receives power on "forward"
-        rather than the motor. Useful when testing your code.
-
+        rather than the motor. Useful when testing your code. 
+        
         Arguments:
         state = boolean
         '''
@@ -45,11 +52,12 @@ class Motor:
 
     def forward(self, speed):
         ''' Starts the motor turning in its configured "forward" direction.
+
         Arguments:
         speed = Duty Cycle Percentage from 0 to 100.
         0 - stop and 100 - maximum speed
-        '''
-        print("Forward")
+        '''    
+        #print("Forward")
         if self.testMode:
             self.arrow.on()
         else:
@@ -59,11 +67,12 @@ class Motor:
 
     def reverse(self,speed):
         ''' Starts the motor turning in its configured "reverse" direction.
+
         Arguments:
         speed = Duty Cycle Percentage from 0 to 100.
         0 - stop and 100 - maximum speed
      '''
-        print("Reverse")
+        #print("Reverse")
         if self.testMode:
             self.arrow.off()
         else:
@@ -74,7 +83,7 @@ class Motor:
     def stop(self):
         ''' Stops power to the motor,
      '''
-        print("Stop")
+        #print("Stop")
         self.arrow.off()
         self.PWM.ChangeDutyCycle(0)
         GPIO.output(self.pins['f'],GPIO.LOW)
@@ -83,11 +92,196 @@ class Motor:
     def speed(self):
         ''' Control Speed of Motor,
      '''
-        pass
+
+class LinkedMotors:
+    ''' Links 2 or more motors together as a set.
+    
+        This allows a single command to be used to control a linked set of motors
+        e.g. For a 4x wheel vehicle this allows a single command to make all 4 wheels go forward.
+        Starts the motor turning in its configured "forward" direction.
+        
+        Arguments:
+        *motors = a list of Motor objects
+     '''
+    def __init__(self, *motors):
+        self.motor = []
+        for i in motors:
+            #print(i.pins)
+            self.motor.append(i)
+
+    def forward(self,speed):
+        ''' Starts the motor turning in its configured "forward" direction.
+
+        Arguments:
+        speed = Duty Cycle Percentage from 0 to 100.
+        0 - stop and 100 - maximum speed 
+     '''
+        for i in range(len(self.motor)):
+            self.motor[i].forward(speed)
+
+    def reverse(self,speed):
+        ''' Starts the motor turning in its configured "reverse" direction.
+
+        Arguments:
+        speed = Duty Cycle Percentage from 0 to 100.
+        0 - stop and 100 - maximum speed
+     '''
+        for i in range(len(self.motor)):
+            self.motor[i].reverse(speed)
+
+    def stop(self):
+        ''' Stops power to the motor,
+     '''
+        for i in range(len(self.motor)):
+            self.motor[i].stop()
+
+
+
+class Stepper:
+    ''' Defines stepper motor pins on the MotorShield
+    
+        Arguments:
+        motor = stepper motor
+    '''
+    
+    stepperpins = {"STEPPER1":{"en1":11, "en2":22, "c1":13,"c2":15, "c3":18, "c4":16},
+                   "STEPPER2":{"en1":19, "en2":32, "c1":21,"c2":23, "c3":24, "c4":26}}
+                  
+    def __init__(self, motor):
+        self.config = self.stepperpins[motor]
+        GPIO.setup(self.config["en1"],GPIO.OUT)
+        GPIO.setup(self.config["en2"],GPIO.OUT)
+        GPIO.setup(self.config["c1"],GPIO.OUT)
+        GPIO.setup(self.config["c2"],GPIO.OUT)
+        GPIO.setup(self.config["c3"],GPIO.OUT)
+        GPIO.setup(self.config["c4"],GPIO.OUT)
+        
+        GPIO.output(self.config["en1"],GPIO.HIGH)
+        GPIO.output(self.config["en2"],GPIO.HIGH)
+        GPIO.output(self.config["c1"],GPIO.LOW)
+        GPIO.output(self.config["c2"],GPIO.LOW)
+        GPIO.output(self.config["c3"],GPIO.LOW)
+        GPIO.output(self.config["c4"],GPIO.LOW)
+
+    ''' Set steps of Stepper Motor
+    
+        Arguments:
+        w1,w2,w3,w4 = Wire of Stepper Motor
+    '''
+    def setStep(self, w1, w2, w3, w4):
+        GPIO.output(self.config["c1"], w1)
+        GPIO.output(self.config["c2"], w2)
+        GPIO.output(self.config["c3"], w3)
+        GPIO.output(self.config["c4"], w4)
+
+    ''' Rotate Stepper motor in forward direction
+    
+        Arguments:
+        delay = time between steps in miliseconds
+        steps = Number of Steps
+    '''
+    def forward(self, delay, steps):
+        for i in range(0, steps):
+            self.setStep(1, 0, 0, 0)
+            time.sleep(delay)
+            self.setStep(0, 1, 0, 0)
+            time.sleep(delay)
+            self.setStep(0, 0, 1, 0)
+            time.sleep(delay)
+            self.setStep(0, 0, 0, 1)
+            time.sleep(delay)
+
+    ''' Rotate Stepper motor in backward direction
+    
+        Arguments:
+        delay = time between steps
+        steps = Number of Steps
+    '''
+    def backward(self, delay, steps):
+        for i in range(0, steps):
+            self.setStep(0, 0, 0, 1)
+            time.sleep(delay)
+            self.setStep(0, 0, 1, 0)
+            time.sleep(delay)
+            self.setStep(0, 1, 0, 0)
+            time.sleep(delay)
+            self.setStep(1, 0, 0, 0)
+            time.sleep(delay)
+
+    def stop(self):
+        ''' Stops power to the motor,
+     '''
+        #print("Stop Stepper Motor")
+        GPIO.output(self.config['c1'],GPIO.LOW)
+        GPIO.output(self.config['c2'],GPIO.LOW)
+        GPIO.output(self.config['c3'],GPIO.LOW)
+        GPIO.output(self.config['c4'],GPIO.LOW)
+        
+
+
+class Sensor:
+    ''' Defines a sensor connected to the sensor pins on the MotorShield
+    
+        Arguments:
+        sensortype = string identifying which sensor is being configured.
+            i.e. "IR1", "IR2", "ULTRASONIC"
+        boundary = an integer specifying the minimum distance at which the sensor
+            will return a Triggered response of True. 
+    '''
+    Triggered = False
+    def iRCheck(self):
+        input_state = GPIO.input(self.config["echo"])
+        if input_state == True:
+            print("Sensor 2: Object Detected")
+            self.Triggered = True
+        else:
+            self.Triggered = False
+
+    def sonicCheck(self):
+        print("SonicCheck has been triggered")
+        time.sleep(0.333)
+        GPIO.output(self.config["trigger"], True)
+        time.sleep(0.00001)
+        GPIO.output(self.config["trigger"], False)
+        start = time.time()
+        while GPIO.input(self.config["echo"])==0:
+            start = time.time()
+        while GPIO.input(self.config["echo"])==1:
+            stop = time.time()
+        elapsed = stop-start
+        measure = (elapsed * 34300)/2
+        self.lastRead = measure
+        if self.boundary > measure:
+            print("Boundary breached")
+            print(self.boundary)
+            print(measure)
+            self.Triggered = True
+        else:
+            self.Triggered = False
+        
+    sensorpins = {"IR1":{"echo":7, "check":iRCheck}, "IR2":{"echo":12, "check":iRCheck},
+                  "ULTRASONIC":{"trigger":29, "echo": 31, "check":sonicCheck}}
+
+    def trigger(self):
+        ''' Executes the relevant routine that activates and takes a reading from the specified sensor.
+    
+        If the specified "boundary" has been breached the Sensor's Triggered attribute gets set to True.
+    ''' 
+        self.config["check"](self)
+        print("Trigger Called")
+
+    def __init__(self, sensortype, boundary):
+        self.config = self.sensorpins[sensortype]
+        self.boundary = boundary
+        self.lastRead = 0
+        if "trigger" in self.config:
+            print("trigger")
+            GPIO.setup(self.config["trigger"],GPIO.OUT)
+        GPIO.setup(self.config["echo"],GPIO.IN) 
 
 class Arrow():
     ''' Defines an object for controlling one of the LED arrows on the Motorshield.
-
+    
         Arguments:
         which = integer label for each arrow. The arrow number if arbitrary starting with:
             1 = Arrow closest to the Motorshield's power pins and running clockwise round the board
@@ -107,6 +301,12 @@ class Arrow():
     def off(self):
         GPIO.output(self.pin,GPIO.LOW)
 
-# if __name__ == "__main__":
-#     drive = Motor("MOTOR1",2)
-#     steer = Motor("MOTOR2", 1)
+
+if __name__ == "__main__":
+    step1 = Stepper("STEPPER1")
+
+    while True:
+        step1.forward(.1,10)
+        time.sleep(2)
+        step1.backward(.1,10)
+        time.sleep(2)
--- main.py
+++ main.py
@@ -1,61 +1,124 @@
+# To convert picamera image to opencv
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from PiMotor import Motor, Arrow
+# Code provided by motor shield
+import PiMotor
+import RPi.GPIO as GPIO
 import time
 import numpy
 import cv2
-
-class Ultrasonic:
-
-    def __init__(self, trig, echo):
-        pass
-
-drive = None
-steer = None
-cam   = None
-cap   = None
-front = None
-left  = None
-right = None
-back  = None
+import random
 
 def main():
-    # Initializing all the variables
-    drive = Motor("MOTOR1",2)
-    steer = Motor("MOTOR2",1)
-
-    # Camera setup
+    # motor for tail
+    tail  = PiMotor.Motor("MOTOR1", 1)
     cam   = PiCamera()
     cam.resolution = (640,480)
     cam.framerate  = 32
     cap   = PiRGBArray(cam)
-
+    # Classifier for front of face. Must be absolute path 
+    # for autoboot
+    classifier = cv2.CascadeClassifier("/home/pi/robot_desire/files/haarcascade_frontalface_default.xml")
     # Camera warm up
     time.sleep(.1)
+    close = -1000
 
-	# Loop to generate images using the picamera
+    # Setting up servo
+    GPIO.setup( 3, GPIO.OUT)
+
+    head = GPIO.PWM( 3, 100)
+    head_ang = 100
+    last_seen = time.time()
+
+    head.start(7.5)
+            
+    # Moves head to desired angle
+    def swervo():
+        duty = float(head_ang) / 10.0 + 2.5
+        head.ChangeDutyCycle(duty)
+
+    # Shaking head to make sure user knows
+    # everything is ready
+    print("waking up")
+    for i in range(5):
+        if i % 2:
+            head_ang = 50
+        else:
+            head_ang = 0
+        swervo()
+        time.sleep(2)
+
+    # Loop to generate images using the picamera
     for frame in cam.capture_continuous(cap, format='bgr', use_video_port=True):
-		# getting the image from frame
-		img = frame.array
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # getting the image from frame
+        img = frame.array
 
-        faces = cv2.CascadeClassifier("files/haarcascade_frontalface_default.xml").detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30,30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
+        # opencv algs are in grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+
+        # List of faces
+        faces = classifier.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30,30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+                )
+
+        if len(faces) != 0:
+            close=faces[0]
+        else:
+            close=-1000
 
         for (x,y,w,h) in faces:
             cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-        cv2.imshow("Face Tracking", img)
-        key = cv2.waitKey(1) & 0xFF
 
-        cap.truncate(0)
+        avg_y = 0
+        # No need to show image unless you have
+        # some form of gui. Jessie lite does not
+        # cv2.imshow("Image", img)
+        closest = 1000000
+        for face in faces:
+            x,y,w,h = face
+            avg_y += y
+            dist = (x-close[0])**2 + (y-close[1])**2
+            if dist < closest:
+                close = x, y, w, h
+                closest = dist
+        if len(faces):
+            avg_y = float(avg_y)/len(faces)
 
-        if key == ord('q'):
-            break
+        if close != -1000:
+            print("seen")
+            last_seen = time.time()
+            
+            # Moving came to center faces
+            if avg_y > 270 and head_ang >=  10:
+                head_ang -= 10
+                print("you're too low")
+            elif avg_y < 210 and head_ang <= 90:
+                head_ang += 10
+                print("you're too high")
+            # move tail if facing someone
+            if len(faces):
+                tail.forward(100)
+        else:
+            # Move head if board, haha
+            if time.time() - last_seen > 10:
+                head_ang = random.randint(50,100)
+                last_seen = time.time()
+                print("moving head")
+            tail.stop()
+
+        swervo()
+        # This is to stop program, however, only
+        # works when GUI is enabled
+        # key = cv2.waitKey(1) & 0xFF
+        #
+        # cap.truncate(0)
+        #
+        # if key == ord('q'):
+        #     break
 
 
 if __name__ == "__main__":
